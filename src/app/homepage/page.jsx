f58369c1a07eaa7bdb6327244c3dbaca6c55e574
Skailodon <jackiehtruong@outlook.com>
2024-11-26 15:12:41 -0800

    Inseerted carousel for news & content position

--- src/app/homepage/page.jsx
+++ src/app/homepage/page.jsx
@@ -9,7 +9,12 @@ import { fetchWeatherData, WeatherData } from "@/utils/fetchWeatherData";
 import { useState, useEffect, use } from "react";
 import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 import { useRouter } from "next/router";
-
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay"
 
 export default function HomePage() {
     const [cityName, setCityName] = useState("Vancouver");
@@ -60,37 +65,68 @@ export default function HomePage() {
                         </header>
                         <h1 className={`font-black ${styles.landingLogo}`}>FLARE</h1>
                     </div>
+                    <div className={`${styles.contentPosition}`}>
+                        <div className={`grid ${styles.contentContainer}`}>
+                            <div className={`grid grid-cols-2 gap-4 ${styles.fireRisk} ${riskColour}`}>
+                                <h2 className={styles.homeHeading}>Wildfire Risk: {fireRisk}</h2>
+                                <CircleHelp size={24} />
+                            </div>
 
-                    <div className={`grid ${styles.contentContainer}`}>
-                        <div className={`grid grid-cols-2 gap-4 ${styles.fireRisk} ${riskColour}`}>
-                            <h2 className={styles.homeHeading}>Wildfire Risk: {fireRisk}</h2>
-                            <CircleHelp size={24} />
+                            <Link href="/map" className={styles.location}>
+                                <h3>{weatherData ? `${weatherData.cityName}` : <p>My Location</p>}</h3>
+                                {weatherData ? (
+                                    <>
+                                        <h1>{weatherData.current.temp.toFixed(0)}°C</h1>
+                                        <div className="flex space-x-6 items-center">
+                                            <p className="">{weatherData.current.weather[0].description}</p>
+                                            <Wind size={32} />
+                                        </div>
+                                    </>
+                                ) : (
+                                    <h1>Loading...</h1>
+                                )}
+                            </Link>
+
+                            <Link href="/safety" className={styles.safety}>
+                                <h3>Safety</h3>
+                                <p>Learn how to prepare for a wildfire. Learn the early signs</p>
+                            </Link>
+
+                            <Link href="/news" className={`${styles.news} ${styles.newsItemOne}`}>
+                                <Carousel
+                                    className="w-full h-full"
+                                    plugins={[
+                                        Autoplay({
+                                            delay: 2000,
+                                        }),
+                                    ]}
+                                >
+                                    <CarouselContent className={`w-full h-full`}>
+                                        <CarouselItem className={`w-full h-full`}>
+                                            <h3>News</h3>
+                                            <p>
+                                                Vancouver’s Unique Coastal Climate: A Balance of Rain and Mild Temperatures: mild,
+                                                wet winters and pleasantly warm summers
+                                            </p>
+                                        </CarouselItem>
+                                        <CarouselItem className={`w-full h-full`}>
+                                            <h3>News</h3>
+                                            <p>
+                                                How Vancouver’s Rain Shapes Urban Life: The city receives around 160 days of rain each
+                                                year...
+                                            </p>
+                                        </CarouselItem>
+                                        <CarouselItem className={`w-full h-full`}>
+                                            <h3>News</h3>
+                                            <p>
+                                                The Best and Worst Seasons to Visit Vancouver: Weather Insights for Travelers: For those
+                                                planning a trip...
+                                            </p>
+                                        </CarouselItem>
+                                    </CarouselContent>
+                                </Carousel>
+                            </Link>
                         </div>
-
-                        <Link href="/map" className={styles.location}>
-                            <h3>{weatherData ? `${weatherData.cityName}` : <p>My Location</p>}</h3>
-                            {weatherData ? (
-                                <>
-                                    <h1>{weatherData.current.temp.toFixed(0)}°C</h1>
-                                    <div className="flex space-x-6 items-center">
-                                        <p className="">{weatherData.current.weather[0].description}</p>
-                                        <Wind size={32} />
-                                    </div>
-                                </>
-                            ) : (
-                                <h1>Loading...</h1>
-                            )}
-                        </Link>
-
-                        <Link href="/safety" className={styles.safety}>
-                            <h3>Safety</h3>
-                            <p>Learn how to prepare for a wildfire. Learn the early signs</p>
-                        </Link>
-
-                        <Link href="/news" className={styles.news}>
-                            <h3>News</h3>
-                            <p>Vancouver’s Unique Coastal Climate: A Balance of Rain and Mild Temperatures: mild, wet winters and pleasantly warm summers</p>
-                        </Link>
                     </div>
                 </div>
                 <BottomNavBar />
