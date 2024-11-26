c64d87d768a3faa2807d6c71f99b7cd8c2818ab8
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2024-11-26 15:54:24 -0800

    nav source title / bottom nav fix

--- src/app/article/articleNavigation.tsx
+++ src/app/article/articleNavigation.tsx
@@ -1,12 +1,15 @@
 import "./page.css";
 import { CircleArrowLeft } from "lucide-react";
 
+interface ArticleNavigationProps {
+    source: string;
+}
 
-export default function ArticleNavigation() {
+export default function ArticleNavigation({ source }: ArticleNavigationProps) {
     return (
         <div className="headColor flex stickyNav justify-between items-center">
             <CircleArrowLeft className="arrow shrink-0" />
-            <h4 className="newsOrg">Coastal News</h4>
+            <h4 className="newsOrg">{source}</h4>
         </div>
     );
 }
--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -33,35 +33,42 @@ export default function Article() {
   if (!articleData) return <div>Loading...</div>;
 
   return (
-    <div className="appLayout flex-col">
-      <div className="flex-col max-w-[850px] mx-auto w-full">
-        <ArticleNavigation />
-        <ArticleInfo 
-          title={articleData.title}
-          date={articleData.date}
-          author={articleData.author}
-          source={articleData.source}
-        />
-        {articleData.isProtected ? (
-          <div className="p-4 text-center text-white">
-            <p className="mb-4">{articleData.content}</p>
-            <a 
-              href={articleData.url} 
-              target="_blank" 
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:text-blue-700 underline"
-            >
-              View Original Article
-            </a>
-          </div>
-        ) : (
-          <>
-            <ArticleHero imageUrl={articleData.image} />
-            <ArticleContent content={articleData.content} />
-          </>
-        )}
+    <div>
+      <div className="appLayout flex-col">
+        <div className="flex-col max-w-[850px] mx-auto w-full">
+          <ArticleNavigation 
+            source={articleData.source}
+          />
+          <ArticleInfo 
+            title={articleData.title}
+            date={articleData.date}
+            author={articleData.author}
+            source={articleData.source}
+          />
+          {articleData.isProtected ? (
+            <div className="p-4 text-center text-white">
+              <p className="mb-4">{articleData.content}</p>
+              <a 
+                href={articleData.url} 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:text-blue-700 underline"
+              >
+                View Original Article
+              </a>
+            </div>
+          ) : (
+            <div className='flex flex-col'>
+              <ArticleHero imageUrl={articleData.image} />
+              <div>
+                <ArticleContent content={articleData.content} />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <BottomNavBar />
     </div>
+
   );
 }
