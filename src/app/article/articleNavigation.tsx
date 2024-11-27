1ac0dc1f19cc5ab6643a0817e084e6774f729a02
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2024-11-26 16:01:35 -0800

    routing form article to news

--- src/app/article/articleNavigation.tsx
+++ src/app/article/articleNavigation.tsx
@@ -1,15 +1,25 @@
 import "./page.css";
 import { CircleArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface ArticleNavigationProps {
-    source: string;
+  source: string;
 }
 
 export default function ArticleNavigation({ source }: ArticleNavigationProps) {
-    return (
-        <div className="headColor flex stickyNav justify-between items-center">
-            <CircleArrowLeft className="arrow shrink-0" />
-            <h4 className="newsOrg">{source}</h4>
-        </div>
-    );
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back(); // This navigates to the previous page
+  };
+
+  return (
+    <div className="headColor flex stickyNav justify-between items-center">
+      <CircleArrowLeft 
+        className="arrow shrink-0 cursor-pointer" 
+        onClick={handleGoBack} 
+      />
+      <h4 className="newsOrg">{source}</h4>
+    </div>
+  );
 }
